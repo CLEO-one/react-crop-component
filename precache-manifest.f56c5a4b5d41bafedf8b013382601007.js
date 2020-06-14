77a909b20c6c54d38c52c31938d77519213ecfb2
Yusuf Özlü <y.ozlu@cleo.one>
2020-06-14 17:23:59 +0200

    Updates

---
+++ precache-manifest.f56c5a4b5d41bafedf8b013382601007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb2d43d7c3d709774c98c76644982cf",
+    "url": "/react-crop-component/index.html"
+  },
+  {
+    "revision": "25b66d42c636b502e4c9",
+    "url": "/react-crop-component/static/css/main.d08edc0e.chunk.css"
+  },
+  {
+    "revision": "f456b94ae3162c998f2f",
+    "url": "/react-crop-component/static/js/2.414b1951.chunk.js"
+  },
+  {
+    "revision": "462f991c1d2ef16102b314f225132ac4",
+    "url": "/react-crop-component/static/js/2.414b1951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b66d42c636b502e4c9",
+    "url": "/react-crop-component/static/js/main.349aa227.chunk.js"
+  },
+  {
+    "revision": "e0a02d9e2b99d840a331",
+    "url": "/react-crop-component/static/js/runtime-main.0f70c159.js"
+  }
+]);
